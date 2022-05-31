12a6b4187c640c81f7ef6eb396cf9e58e24bfb55
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2022-05-31 02:12:07 +0000

    Create and Read with mongoDB and embedded JS

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ This can be used to test node modules or applications which require external ser
 You need the following environment variables
 - PORT
 
-This can be set by the dotenv module `npm install -D dotenv` then run the node server via `npm start dev`
+This can be set by the dotenv module `npm install -D dotenv` , create a `.env` file similar to the example and then run the node server via `npm run dev` (because we have the dev script in package.json)
--- server.js
+++ server.js
@@ -1,14 +1,59 @@
+// imports
 const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+// globals
 const app = express()
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const PORT = process.env.PORT || 8000
+const DB_NAME = process.env.DB_NAME || "rickyisms"
+const DB_CONNECTION = process.env.DB_CONNECTION || "mongodb://127.0.0.1:27017/"
 
+// server message
 console.log('Avengers of the Nerds')
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`))
+// Setup DB
+MongoClient.connect(DB_CONNECTION)
+    .then(client => {
+        console.log("Connected to DB")
+        const db = client.db(DB_NAME)
+        const quotesCollection = db.collection("quotes")
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+        // template engine
+        app.set('view engine', 'ejs')
+
+        // middleware
+        app.use(bodyParser.urlencoded({ extended: true }))
+
+        // handlers
+        app.get('/', (req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(error => console.error(error))
+        })
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        // server start
+        app.listen(PORT, () => console.log(`listening on ${PORT}`))
+    })
+    .catch(error => console.error(error))
+
+// Handlers
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
+// })
